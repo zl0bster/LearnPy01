06af2b2e7284cbea970167de6f5b12c28fa7c1c5
Zl0bster <s00per@ya.ru>
2021-01-08 00:48:52 +0300

    turns model and draws it

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 # "python pip install numpy"
-COMMAND = "python Task_01.py"
+# COMMAND = "python model_manage.py -x 1800 -y 1000 -file LK1-002.01c.STL"
+COMMAND = "python model_manage.py -x 1800 -y 1000 -file Dispenser.stl"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- model_manage.py
+++ model_manage.py
@@ -1,3 +1,6 @@
+import argparse
+from typing import Sequence
+
 import numpy as np
 import simple_draw as sd
 
@@ -10,6 +13,71 @@ Z = 2
 R = 0
 A = 1
 E = 2
+
+
+def main():
+    parser = parserDefinition()
+    args = parser.parse_args()
+    xResolution = args.xres
+    yResolution = args.yres
+    sd.resolution = (xResolution, yResolution)
+    xCenter = xResolution / 2
+    yCenter = yResolution / 2
+    stlFile = args.file.name  # 'LK1-002.01c.STL'
+    model = model_create(stlFile)
+    vertex = model.get_vxs_np_array()
+    bodyCenter = focuse_point_count(vertex)
+    print(bodyCenter)
+    vxsXYZcentered = array_plus_point(pts=vertex,
+                                      pt=(-bodyCenter[X], -bodyCenter[Y], -bodyCenter[Z]))
+    vxsDAEbodyCenter = arrayXYZtoDAE(vxsXYZcentered)
+    deltaA = 00  # degrees
+    deltaE = 00  # degrees
+    scale = 10
+    currentBodyVxsDAE = array_plus_point(pts=vxsDAEbodyCenter,
+                                         pt=(0, np.radians(deltaA), np.radians(deltaE)))
+    # currentBodyVxsDAE = vxsDAEbodyCenter
+    screenProjectionVXsXYZ = np.round(arrayDAEtoXYZ(currentBodyVxsDAE))
+    screenProjectionVXsXYZ = vertex
+    envBox = envelope_box_count(screenProjectionVXsXYZ)
+    print(envBox)
+    screenProjectionVXsXYZ = np.multiply(screenProjectionVXsXYZ, scale)
+    screenProjectionVXsXYZ = array_plus_point(pts=screenProjectionVXsXYZ,
+                                              pt=(xCenter, yCenter, 0))
+    envBox = envelope_box_count(screenProjectionVXsXYZ)
+    print(envBox)
+    sd._init()
+    draw_model_1(model=model, screenVXs=screenProjectionVXsXYZ)
+    while not sd.user_want_exit():
+        continue
+    sd.quit()
+
+
+def parserDefinition():
+    """
+    command line arguments parse to adjust screen object parameters
+    :return:
+    parser data structure
+    screen resolution
+    parser.xres : int  - screen resolution
+    parser.yres : int  - screen resolution
+    parser.file : str  - configuration file name
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-x', '--xres', help='window x resolution', type=int, default=1600)
+    parser.add_argument('-y', '--yres', help='window y resolution', type=int, default=950)
+    parser.add_argument('-file', type=argparse.FileType('r'), help='scene config file path', required=True)
+    return parser
+
+
+def draw_model_1(model: dd.BodyFaces, screenVXs):
+    edgesList = model.get_all_edges()
+    sd.start_drawing()  # removes  blinking
+    for edge in edgesList:
+        pt1 = sd.get_point(screenVXs[edge[0], X], screenVXs[edge[0], Y])
+        pt2 = sd.get_point(screenVXs[edge[1], X], screenVXs[edge[1], Y])
+        sd.line(pt1, pt2)
+    sd.finish_drawing()  # removes  blinking
 
 
 def model_create(name: str) -> dd.BodyFaces:
@@ -38,6 +106,9 @@ def envelope_box_count(points):  # np vertex array
 def focuse_point_count(points):  # np vertex array
     """Counts center coord of envelope"""
     xmin, ymin, zmin, xmax, ymax, zmax = envelope_box_count(points)
+    xmin = abs(xmin) if xmin < 0 else -xmin
+    ymin = abs(ymin) if ymin < 0 else -ymin
+    zmin = abs(zmin) if zmin < 0 else -zmin
     x = (xmax + xmin) / 2
     y = (ymax + ymin) / 2
     z = (zmax + zmin) / 2
@@ -72,10 +143,27 @@ def arrayDAEtoXYZ(pts):  # np vertex array in DAE
     return result
 
 
+def pointsXYZdelta(pnt1: Sequence[float], pnt2: Sequence[float]) -> Sequence[float]:
+    x = pnt2[X] - pnt1[X]
+    y = pnt2[Y] - pnt1[Y]
+    z = pnt2[Z] - pnt1[Z]
+    return x, y, z
+
+
+def array_plus_point(pts, pt: Sequence[float]):  # np vertex array in XYZ or DAE
+    """Transforms decart coordinates array to angular"""
+    result = np.zeros(pts.shape)
+    for i in range(pts.shape[0]):
+        result[i, X] = pts[i, X] + pt[X]
+        result[i, Y] = pts[i, Y] + pt[Y]
+        result[i, Z] = pts[i, Z] + pt[Z]
+    return result
+
+
 if __name__ == '__main__':
-    stlFile = 'LK1-002.01c.STL'
-    model = model_create(stlFile)
-    vertex = model.get_vxs_np_array()
+    # stlFile = 'LK1-002.01c.STL'
+    # model = model_create(stlFile)
+    # vertex = model.get_vxs_np_array()
     # print(len(model))
     # print(model.get_vxs_np_array())
     # print(model.get_all_edges())
@@ -86,13 +174,15 @@ if __name__ == '__main__':
     #     print(i)
     #     print(model.get_face_edges(i))
     #     print(model.get_face_vertexes(i))
-    print(vertex)
+    # print(vertex)
     # print(envelope_box_count(vertex))
     # print((focuse_point_count(vertex)))
-    sd._init()
-    print(vertex.shape[0])
+
+    # print(vertex.shape[0])
     # print(vertex.size)
     # for i in range(vertex.shape[0]):
     #     print(vertex[i])
-    vxDAE = arrayXYZtoDAE(vertex)
-    print(np.subtract(vertex, arrayDAEtoXYZ(vxDAE)))
+    # vxDAE = arrayXYZtoDAE(vertex)
+    # # print(np.subtract(vertex, arrayDAEtoXYZ(vxDAE)))
+    # print(array_plus_point(pts=vertex, pt=(10, -10, 10)))
+    main()
