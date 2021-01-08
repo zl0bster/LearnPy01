e837957ba8e27b9f908d1ef03ff3c49e3af72303
Zl0bster <s00per@ya.ru>
2021-01-08 19:22:21 +0300

    vertex manipulation functions separated to file

--- model_manage.py
+++ model_manage.py
@@ -6,6 +6,7 @@ import simple_draw as sd
 
 import data_def as dd
 import file_read as fr
+import vertex_manipulations as vm
 
 X = 0
 Y = 1
@@ -26,25 +27,25 @@ def main():
     stlFile = args.file.name  # 'LK1-002.01c.STL'
     model = model_create(stlFile)
     vertex = model.get_vxs_np_array()
-    bodyCenter = focuse_point_count(vertex)
+    bodyCenter = vm.focuse_point_count(vertex)
     print(bodyCenter)
-    vxsXYZcentered = array_plus_point(pts=vertex,
+    vxsXYZcentered = vm.array_plus_point(pts=vertex,
                                       pt=(-bodyCenter[X], -bodyCenter[Y], -bodyCenter[Z]))
-    vxsDAEbodyCenter = arrayXYZtoDAE(vxsXYZcentered)
+    vxsDAEbodyCenter = vm.arrayXYZtoDAE(vxsXYZcentered)
     deltaA = 00  # degrees
     deltaE = 00  # degrees
     scale = 10
-    currentBodyVxsDAE = array_plus_point(pts=vxsDAEbodyCenter,
+    currentBodyVxsDAE = vm.array_plus_point(pts=vxsDAEbodyCenter,
                                          pt=(0, np.radians(deltaA), np.radians(deltaE)))
-    # currentBodyVxsDAE = vxsDAEbodyCenter
-    screenProjectionVXsXYZ = np.round(arrayDAEtoXYZ(currentBodyVxsDAE))
-    screenProjectionVXsXYZ = vertex
-    envBox = envelope_box_count(screenProjectionVXsXYZ)
+    currentBodyVxsDAE = vxsDAEbodyCenter
+    screenProjectionVXsXYZ = np.round(vm.arrayDAEtoXYZ(currentBodyVxsDAE))
+    # screenProjectionVXsXYZ = vertex
+    envBox = vm.envelope_box_count(screenProjectionVXsXYZ)
     print(envBox)
     screenProjectionVXsXYZ = np.multiply(screenProjectionVXsXYZ, scale)
-    screenProjectionVXsXYZ = array_plus_point(pts=screenProjectionVXsXYZ,
+    screenProjectionVXsXYZ = vm.array_plus_point(pts=screenProjectionVXsXYZ,
                                               pt=(xCenter, yCenter, 0))
-    envBox = envelope_box_count(screenProjectionVXsXYZ)
+    envBox = vm.envelope_box_count(screenProjectionVXsXYZ)
     print(envBox)
     sd._init()
     draw_model_1(model=model, screenVXs=screenProjectionVXsXYZ)
@@ -91,73 +92,6 @@ def model_create(name: str) -> dd.BodyFaces:
         body.add_face(facePoints=faceVXses)
     return body
 
-
-def envelope_box_count(points):  # np vertex array
-    """Finds max and min coords in all axes to count envelope"""
-    xmax = np.max(points[:, 0])
-    ymax = np.max(points[:, 1])
-    zmax = np.max(points[:, 2])
-    xmin = np.min(points[:, 0])
-    ymin = np.min(points[:, 1])
-    zmin = np.min(points[:, 2])
-    return xmin, ymin, zmin, xmax, ymax, zmax
-
-
-def focuse_point_count(points):  # np vertex array
-    """Counts center coord of envelope"""
-    xmin, ymin, zmin, xmax, ymax, zmax = envelope_box_count(points)
-    xmin = abs(xmin) if xmin < 0 else -xmin
-    ymin = abs(ymin) if ymin < 0 else -ymin
-    zmin = abs(zmin) if zmin < 0 else -zmin
-    x = (xmax + xmin) / 2
-    y = (ymax + ymin) / 2
-    z = (zmax + zmin) / 2
-    return x, y, z
-
-
-def arrayXYZtoDAE(pts):  # np vertex array in XYZ
-    """Transforms decart coordinates array to angular"""
-    result = np.zeros(pts.shape)
-    for i in range(pts.shape[0]):
-        if pts[i].any() == 0:
-            continue
-        result[i, R] = np.sqrt(np.power(pts[i, X], 2) + np.power(pts[i, Y], 2) + np.power(pts[i, Z], 2))
-        result[i, E] = np.arcsin(pts[i, Z] / result[i, R])
-        lenXY = np.hypot(pts[i, X], pts[i, Y])
-        if lenXY == 0:
-            result[i, A] = np.pi / 2
-        else:
-            result[i, A] = np.arcsin(pts[i, Y] / lenXY)
-    return result
-
-
-def arrayDAEtoXYZ(pts):  # np vertex array in DAE
-    """Transforms angular coordinates array to decart"""
-    result = np.zeros(pts.shape)
-    for i in range(pts.shape[0]):
-        if pts[i].any() == 0:
-            continue
-        result[i, X] = pts[i, R] * np.cos(pts[i, E]) * np.cos(pts[i, A])
-        result[i, Y] = pts[i, R] * np.cos(pts[i, E]) * np.sin(pts[i, A])
-        result[i, Z] = pts[i, R] * np.sin(pts[i, E])
-    return result
-
-
-def pointsXYZdelta(pnt1: Sequence[float], pnt2: Sequence[float]) -> Sequence[float]:
-    x = pnt2[X] - pnt1[X]
-    y = pnt2[Y] - pnt1[Y]
-    z = pnt2[Z] - pnt1[Z]
-    return x, y, z
-
-
-def array_plus_point(pts, pt: Sequence[float]):  # np vertex array in XYZ or DAE
-    """Transforms decart coordinates array to angular"""
-    result = np.zeros(pts.shape)
-    for i in range(pts.shape[0]):
-        result[i, X] = pts[i, X] + pt[X]
-        result[i, Y] = pts[i, Y] + pt[Y]
-        result[i, Z] = pts[i, Z] + pt[Z]
-    return result
 
 
 if __name__ == '__main__':
---
+++ vertex_manipulations.py
@@ -0,0 +1,78 @@
+from typing import Sequence
+
+import numpy as np
+
+import data_def as dd
+X = 0
+Y = 1
+Z = 2
+R = 0
+A = 1
+E = 2
+
+def envelope_box_count(points):  # np vertex array
+    """Finds max and min coords in all axes to count envelope"""
+    xmax = np.max(points[:, 0])
+    ymax = np.max(points[:, 1])
+    zmax = np.max(points[:, 2])
+    xmin = np.min(points[:, 0])
+    ymin = np.min(points[:, 1])
+    zmin = np.min(points[:, 2])
+    return xmin, ymin, zmin, xmax, ymax, zmax
+
+
+def focuse_point_count(points):  # np vertex array
+    """Counts center coord of envelope"""
+    xmin, ymin, zmin, xmax, ymax, zmax = envelope_box_count(points)
+    xmin = abs(xmin) if xmin < 0 else -xmin
+    ymin = abs(ymin) if ymin < 0 else -ymin
+    zmin = abs(zmin) if zmin < 0 else -zmin
+    x = (xmax + xmin) / 2
+    y = (ymax + ymin) / 2
+    z = (zmax + zmin) / 2
+    return x, y, z
+
+
+def arrayXYZtoDAE(pts):  # np vertex array in XYZ
+    """Transforms decart coordinates array to angular"""
+    result = np.zeros(pts.shape)
+    for i in range(pts.shape[0]):
+        if pts[i].any() == 0:
+            continue
+        result[i, R] = np.sqrt(np.power(pts[i, X], 2) + np.power(pts[i, Y], 2) + np.power(pts[i, Z], 2))
+        result[i, E] = np.arcsin(pts[i, Z] / result[i, R])
+        lenXY = np.hypot(pts[i, X], pts[i, Y])
+        if lenXY == 0:
+            result[i, A] = np.pi / 2
+        else:
+            result[i, A] = np.arcsin(pts[i, Y] / lenXY)
+    return result
+
+
+def arrayDAEtoXYZ(pts):  # np vertex array in DAE
+    """Transforms angular coordinates array to decart"""
+    result = np.zeros(pts.shape)
+    for i in range(pts.shape[0]):
+        if pts[i].any() == 0:
+            continue
+        result[i, X] = pts[i, R] * np.cos(pts[i, E]) * np.cos(pts[i, A])
+        result[i, Y] = pts[i, R] * np.cos(pts[i, E]) * np.sin(pts[i, A])
+        result[i, Z] = pts[i, R] * np.sin(pts[i, E])
+    return result
+
+
+def pointsXYZdelta(pnt1: Sequence[float], pnt2: Sequence[float]) -> Sequence[float]:
+    x = pnt2[X] - pnt1[X]
+    y = pnt2[Y] - pnt1[Y]
+    z = pnt2[Z] - pnt1[Z]
+    return x, y, z
+
+
+def array_plus_point(pts, pt: Sequence[float]):  # np vertex array in XYZ or DAE
+    """Transforms decart coordinates array to angular"""
+    result = np.zeros(pts.shape)
+    for i in range(pts.shape[0]):
+        result[i, X] = pts[i, X] + pt[X]
+        result[i, Y] = pts[i, Y] + pt[Y]
+        result[i, Z] = pts[i, Z] + pt[Z]
+    return result
